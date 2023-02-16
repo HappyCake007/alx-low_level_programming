f2998b4c9b2d02e811ff865ad153570ab694715a
diane uwimana <diane.uwimana@gmail.com>
2023-02-16 10:58:09 -0800

    puts finction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  this program will ptrint a string on the console using the puts function
+ * return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
